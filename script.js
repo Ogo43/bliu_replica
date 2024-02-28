11e9917b241f893921c6e4aa32ac02282426f862
Ogo43 <emehaogochukwu@gmail.com>
2024-02-28 13:33:25 -0800

    My first commit

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const questions = document.querySelectorAll(".questionaire");
+// const questions = document.querySelectorAll(".questionaire");
 
-questions.forEach((questionaire) => {
-  questionaire.addEventListener("click", () => {
-    questionaire.classList.toggle("active");
-  });
-});
+// questions.forEach((questionaire) => {
+//   questionaire.addEventListener("click", () => {
+//     questionaire.classList.toggle("active");
+//   });
+// });
+
+// let navList = document.querySelector(".mobile_nav_links");
+// let menuBar = document.querySelector(".icon-burger");
+
+// icon-burger.addEventListener('click', () => {
+//     console.log(navList);
+//     navList.classList.toggle('shown');
+// })
+
+
+
